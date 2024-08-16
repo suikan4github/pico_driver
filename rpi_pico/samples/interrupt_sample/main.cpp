2cbf34412ba20b4f321ef3e8d4109d56526c5dbc
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-08-16 12:44:49 +0900

    PicoWrapper works well on RasPi Pico.

--- rpi_pico/samples/interrupt_sample/main.cpp
+++ rpi_pico/samples/interrupt_sample/main.cpp
@@ -1,21 +1,22 @@
 // Include local definition
-#include "duration.hpp"
-#include "outputpin.hpp"
+#include "picowrapper.hpp"
 
 int main() {
+  PicoWrapper sdk;
+
   // Use RasPi Pico on-board LED.
   // 1=> Turn on, 0 => Turn pff.
   const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
-  // Initializing the GPIO pin and set it output.
-  initialize_outputpin(LED_PIN);
+  sdk.gpio_init(LED_PIN);
+  sdk.gpio_set_dir(LED_PIN, true);
 
   while (true) {
     // Turn LED on
-    put_outputpin(LED_PIN, 1);
-    sleep_ms(duration());
+    sdk.gpio_put(LED_PIN, true);
+    sdk.sleep_ms(200);
     // Turn LED off
-    put_outputpin(LED_PIN, 0);
-    sleep_ms(duration());
+    sdk.gpio_put(LED_PIN, false);
+    sdk.sleep_ms(800);
   }
 }
