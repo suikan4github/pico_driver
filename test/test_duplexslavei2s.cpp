32a5c3540932b26dd972f0765bd819ebcab3659d
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-09-04 17:24:52 +0900

    Test case SDKWrapper.Constructor3() has done.
    - [x] Implement DuplexSlaveI2S::Constructor3().
    - [x] Create Test case SDKWrapper.Constructor3() to fail.
    - [x] Make it success.
    - [x] Remove the duplication.

--- test/test_duplexslavei2s.cpp
+++ test/test_duplexslavei2s.cpp
@@ -1,15 +1,18 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <random>
+
 #include "duplexslavei2s.hpp"
 #include "sdkwrapper.hpp"
 
 class DuplexSlaveI2STest : public ::testing::Test {
  protected:
+  std::random_device rng;
   ::pico_driver::MockSDKWrapper sdk_;
-  PIO pio_ = 23;
-  uint32_t sm_ = 3;
-  uint pin_base_ = 53;
+  PIO pio_ = rng();
+  uint32_t sm_ = rng();
+  uint pin_base_ = rng();
   ::pico_driver::DuplexSlaveI2S *i2s_;
 };
 
@@ -25,3 +28,16 @@ TEST_F(DuplexSlaveI2STest, Constructor4) {
   delete (i2s_);
 
 }  // TEST_F(DuplexSlaveI2STest, Constructor)
+
+// Test 3 parameters constructor.
+TEST_F(DuplexSlaveI2STest, Constructor3) {
+  using ::testing::_;
+
+  EXPECT_CALL(sdk_, pio_sm_claim(_, _)).Times(0);
+  EXPECT_CALL(sdk_, pio_claim_unused_sm(pio_, true));
+
+  i2s_ = new ::pico_driver::DuplexSlaveI2S(sdk_, pio_, pin_base_);
+
+  delete (i2s_);
+
+}  // TEST_F(DuplexSlaveI2STest, Constructor3)
