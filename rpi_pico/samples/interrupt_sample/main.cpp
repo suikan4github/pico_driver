2f6c14feec6621e22525e55c3f07f295f5eb56d5
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-09-07 21:45:48 +0900

    Refactor the code.
    Added comment.
    Move the position of the variable declaration.

--- rpi_pico/samples/interrupt_sample/main.cpp
+++ rpi_pico/samples/interrupt_sample/main.cpp
@@ -21,6 +21,8 @@ int main() {
   const unsigned int i2c_sda_pin = 6;
   const unsigned int mclock = 12'000'000;  // Hz
   const unsigned int fs = 48'000;          // Hz
+  PIO i2s_pio = pio0;
+  const uint i2s_sm = 0;
 
   /*
    * Pin usage of I2S. These pins must be consecutive.
@@ -43,14 +45,16 @@ int main() {
   //     pico_enable_stdio_uart($ { PROJECT_NAME } 0)
   sdk.stdio_init_all();
 
+  // Information for picotool.
   bi_decl(bi_program_description(
       "Working with UMB-ADAU1361A board. ADAU1361A I2C address is 0x38."));
   bi_decl(bi_program_url("https://github.com/suikan4github/duplex-i2s-pico"));
   bi_decl(bi_2pins_with_func(i2c_scl_pin, i2c_sda_pin, GPIO_FUNC_I2C));
-  bi_decl(bi_4pins_with_names(
-      I2S_GPIO_PIN_BASE, "I2S SDO", I2S_GPIO_PIN_BASE + 1, "I2S_SDI",
-      I2S_GPIO_PIN_BASE + 2, "I2S BCLK", I2S_GPIO_PIN_BASE + 3, "I2S WS"));
-  bi_decl(bi_1pin_with_name(I2S_GPIO_PIN_DEBUG, "DEBUG"));
+  bi_decl(bi_4pins_with_names(I2S_GPIO_PIN_BASE, "I2S SDO",
+                              I2S_GPIO_PIN_BASE + 1, "I2S_SDI",
+                              I2S_GPIO_PIN_BASE + 2, "I2S BCLK IN",
+                              I2S_GPIO_PIN_BASE + 3, "I2S WS IN"));
+  bi_decl(bi_1pin_with_name(I2S_GPIO_PIN_DEBUG, "DEBUG OUT"));
 
 #ifdef I2S_DEBUG
   // Delay cound down to connect the serial termail for debugging.
@@ -86,16 +90,8 @@ int main() {
 
   // I2S Initialization. We run the I2S PIO program from here.
   // We have to wait for the RX FIFO ASAP.
-  PIO i2s_pio = pio0;
-  uint i2s_sm = 0;
-#if 0
-  uint i2s_offset = sdk.pio_add_program(i2s_pio, &duplex_slave_i2s_program);
-  ::duplex_slave_i2s_program_init(i2s_pio, i2s_sm, i2s_offset,
-                                  I2S_GPIO_PIN_BASE);
-#else
-  ::pico_driver::DuplexSlaveI2S i2s(sdk, i2s_pio, I2S_GPIO_PIN_BASE);
+  ::pico_driver::DuplexSlaveI2S i2s(sdk, i2s_pio, i2s_sm, I2S_GPIO_PIN_BASE);
   i2s.Start();
-#endif
 
   // Audio talk thorough
   while (true) {
