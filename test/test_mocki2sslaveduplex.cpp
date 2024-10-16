186ae14b8e0d2c61568d92ada8f691d44dddae5f
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2024-10-17 08:12:55 +0900

    Suppress uninteresting call warning.
    Added forgotten EXPECT_CALL().

--- test/test_mocki2sslaveduplex.cpp
+++ test/test_mocki2sslaveduplex.cpp
@@ -9,11 +9,18 @@
 #include "i2sslaveduplex.hpp"
 #include "sdkwrapper.hpp"
 
+using ::testing::_;
 class MockI2sSlaveDuplexTest : public ::testing::Test {
  protected:
-  virtual void SetUp() { i2s = new rpp_driver::MockI2sSlaveDuplex(sdk_); }
+  virtual void SetUp() {
+    EXPECT_CALL(sdk_, pio_claim_unused_sm(_, _));
+    i2s = new rpp_driver::MockI2sSlaveDuplex(sdk_);
+  }
 
-  virtual void TearDown() { delete i2s; }
+  virtual void TearDown() {
+    EXPECT_CALL(sdk_, pio_sm_unclaim(_, _));
+    delete i2s;
+  }
 
   ::rpp_driver::MockSdkWrapper sdk_;
   ::rpp_driver::MockI2sSlaveDuplex *i2s;
