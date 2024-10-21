c9311f0af3b70643943082285552b99c341bb237
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2024-10-21 16:19:48 +0900

    Issue #23 :  Make SdkWrapper member functions and SDK API Stub conditional.
    Wrap each "modules" by if __has_include <xxx/yyy.h>. So, SdkWrapper will have
    only essential API member function.

--- src/sdk/pico_sdk_headers.h
+++ src/sdk/pico_sdk_headers.h
@@ -6,9 +6,26 @@
  *
  * Not intended to be read from the application program.
  */
-#include <../../hardware_i2c/include/hardware/i2c.h>
-#include <../../hardware_pio/include/hardware/pio.h>
-#include <../../hardware_gpio/include/hardware/gpio.h>
-#include <../../hardware_clocks/include/hardware/clocks.h>
-#include <../../pico_time/include/pico/time.h>
-#include <../../pico_stdio/include/pico/stdio.h>
+#if __has_include(<hardware/i2c.h>) || __has_include(<gmock/gmock.h>)
+#include <hardware/i2c.h>
+#endif  //  __has_include(<hardware/i2c.h>) || __has_include(<gmock/gmock.h>)
+
+#if __has_include(<hardware/pio.h>) || __has_include(<gmock/gmock.h>)
+#include <hardware/pio.h>
+#endif  //  __has_include(<hardware/pio.h>) || __has_include(<gmock/gmock.h>)
+
+#if __has_include(<hardware/gpio.h>) || __has_include(<gmock/gmock.h>)
+#include <hardware/gpio.h>
+#endif  //  __has_include(<hardware/gpio.h>) || __has_include(<gmock/gmock.h>)
+
+#if __has_include(<hardware/clocks.h>) || __has_include(<gmock/gmock.h>)
+#include <hardware/clocks.h>
+#endif  //  __has_include(<hardware/clocks.h>) || __has_include(<gmock/gmock.h>)
+
+#if __has_include(<pico/time.h>) || __has_include(<gmock/gmock.h>)
+#include <pico/time.h>
+#endif  //  __has_include(<pico/time.h>) || __has_include(<gmock/gmock.h>)
+
+#if __has_include(<pico/stdio.h>) || __has_include(<gmock/gmock.h>)
+#include <pico/stdio.h>
+#endif  //  __has_include(<pico/stdio.h>) || __has_include(<gmock/gmock.h>)
